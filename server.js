9d184479b8c2c10b6fee51f656de0b36c380d2e9
gustavo-tp <gustavo1190.gt@gmail.com>
2020-04-13 15:00:15 -0300

    Created project folowing WorkShop Rocktseat

---
+++ server.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const server = express();
+
+const db = require('./db');
+
+const ideas = [
+  {
+    img: 'https://image.flaticon.com/icons/svg/2729/2729048.svg',
+    title: 'Recortes',
+    category: 'Criatividade',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    url: 'http://rocketseat.com.br',
+  },
+];
+
+server.use(express.static('public'));
+
+server.use(express.urlencoded({ extended: true }));
+
+const nunjucks = require('nunjucks');
+nunjucks.configure('views', {
+  express: server,
+  noCache: true,
+});
+
+server.get('/', (req, res) => {
+  db.all(`SELECT * FROM ideas`, (error, rows) => {
+    if (error) {
+      console.log(error);
+      res.send('Erro no banco de dados');
+    }
+
+    const lastIdeas = [...rows].reverse().slice(0, 3);
+
+    return res.render('index.html', { ideas: lastIdeas });
+  });
+});
+
+server.get('/ideias', (req, res) => {
+  db.all(`SELECT * FROM ideas`, (error, rows) => {
+    if (error) {
+      console.log(error);
+      res.send('Erro no banco de dados');
+    }
+
+    return res.render('ideias.html', { ideas: [...rows].reverse() });
+  });
+});
+
+server.post('/', (req, res) => {
+  const query = `
+    INSERT INTO ideas(
+      image,
+      title,
+      category,
+      description,
+      link
+    ) VALUES (?,?,?,?,?);
+  `;
+
+  const values = [
+    req.body.image,
+    req.body.title,
+    req.body.category,
+    req.body.description,
+    req.body.link,
+  ];
+
+  db.run(query, values, (error) => {
+    if (error) {
+      console.log(error);
+      res.send('Erro no banco de dados');
+    }
+
+    return res.redirect('/ideias');
+  });
+});
+
+server.listen(3000);
